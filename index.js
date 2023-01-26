5484db81395df0b305dcaaaf595b9f1e3b57772d
BpynnWork <123020114+BpynnWork@users.noreply.github.com>
2023-01-26 15:06:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Superman is sick!');
+  res.end('Deadpool is my fav!');
 }).listen(8080);
