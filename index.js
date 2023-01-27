0a8eceec25966d967d8875f864fb1217e4ba4a4d
BpynnWork <123020114+BpynnWork@users.noreply.github.com>
2023-01-27 09:09:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('This is me being SMART AF');
+  res.end('Hello PHAC World!');
 }).listen(8080);
